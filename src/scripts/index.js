9090c3edbab700237a584269efca46459e37b758
Felipe Dalbert <felipeoficial195@gmail.com>
2023-03-21 23:47:47 -0300

    corrgindo operador null aware

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,7 +12,7 @@ function getUserProfile(){
         <img src"${userData.avatar_url}" alt="Pic profile"/>
         <div class="data">
             <h1>${userData.name ?? 'Não possuí nome cadastrado 🥺'}</h1>
-            <p>${userData.bio} ?? 'Não possuí bio cadastrada 😢'</p>
+            <p>${userData.bio ?? 'Não possuí bio cadastrada 😢'}</p>
         </div>
     `
 }
