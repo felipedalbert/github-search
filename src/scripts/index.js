9c6f16a6e9d7071b2aaab008e921994d209a80d6
Felipe Dalbert <felipeoficial195@gmail.com>
2023-03-22 23:11:58 -0300

    corrigindo bug de imagem na tag src

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,7 +13,7 @@ function getUserProfile(userName){
         console.log(userData)
 
         let userInfo = `
-        <img src"${userData.avatar_url}" alt="Pic profile"/>
+        <img src="${userData.avatar_url}" alt="Pic profile">
         <div class="data">
             <h1>${userData.name ?? 'Não possuí nome cadastrado 🥺'}</h1>
             <p>${userData.bio ?? 'Não possuí bio cadastrada 😢'}</p>
